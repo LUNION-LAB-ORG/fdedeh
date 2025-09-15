b8743587ed5d2a3c8fdbc8db49c104c56cf7ef75
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-09-15 17:28:54 +0000

    Correction de la récupération des données de l'article et mise à jour des métadonnées pour l'URL et les images

--- app/[locale]/(public)/articles/[slug]/page.tsx
+++ app/[locale]/(public)/articles/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import { obtenirUnArticleAction } from "@/features/articles/actions/article.action";
 import ArticleDetails from "@/features/articles/components/article/article-details";
 import { prefetchArticleQuery } from "@/features/articles/queries/article-detail.query";
 import { IArticle } from '@/features/articles/types/article.type';
 import { addDomainToBackendImagePath } from '@/utils/image-utils';
 import { dehydrate } from "@tanstack/react-query";
+import { ar } from "date-fns/locale";
 
 type Props = {
 	params: Promise<{ slug: string }>
@@ -13,25 +15,21 @@ export async function generateMetadata({ params }: Props) {
 	const { prefetch, queryClient } = prefetchArticleQuery(slug);
 	await prefetch;
 
-	const article = (dehydrate(queryClient).queries[0].state.data as any).data as IArticle;
+	const article = await obtenirUnArticleAction(slug)
+		.then(res => res.data)
+		.catch(() => null);
 
-	return {
+	return article ? {
 		title: article?.title,
 		description: 'Lire cet article sur Fdedeh',
 		openGraph: {
 			title: `${article?.title} - Fdedeh`,
 			description: 'Lire cet article sur Fdedeh',
 			type: 'article',
-			url: `https://fdedeh.com/articles/${article?.id}`,
-			image: addDomainToBackendImagePath(article?.path_resource),
+			url: `https://fdedeh.com/articles/${article?.slug}`,
+			images: addDomainToBackendImagePath(article?.path_resource),
 		},
-		twitter: {
-			card: 'summary_large_image',
-			title: `${article?.title} - Fdedeh`,
-			description: 'Lire cet article sur Fdedeh',
-			image: addDomainToBackendImagePath(article?.path_resource),
-		},
-	};
+	} : {};
 }
 
 async function ArticleDetailPage({ params }: Props) {
