c8ca312e34b2e1fb9275196a8a37b7ae1d31711f
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-10-17 06:58:14 +0000

    changement du popup

--- features/banner/components/banner-dialog.tsx
+++ features/banner/components/banner-dialog.tsx
@@ -33,7 +33,7 @@ export default function BannerDialog() {
           <AlertDialogTitle className="sr-only">Bienvenue sur FDedeh!</AlertDialogTitle>
         </AlertDialogHeader>
         <Image
-          src="/images/popup/2.png"
+          src="/images/popup/3.png"
           alt="Banner"
           className="aspect-square"
           width={650}
