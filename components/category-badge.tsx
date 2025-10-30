f607e223503b40b7f85d036243bf777b9cb6922a
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2025-10-30 09:48:21 +0000

    Ajout d'une vérification pour éviter l'affichage d'un badge vide dans CategoryBadge

--- components/category-badge.tsx
+++ components/category-badge.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Badge} from "@/components/ui/badge";
-import {cn} from "@/lib/utils";
-import {getCategoryColor} from "@/features/articles/utils/category-colors";
-import {ICategorie} from "@/features/categories/types/categorie.type";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+import { getCategoryColor } from "@/features/articles/utils/category-colors";
+import { ICategorie } from "@/features/categories/types/categorie.type";
 
-function CategoryBadge({category, className}: { category: ICategorie, className: string }) {
+function CategoryBadge({ category, className }: { category: ICategorie, className: string }) {
+	if (!category) return null;
 	return (
 		<Badge className={cn(
 			"first-letter:capitalize text-white rounded-2xl p-2 text-xs",
